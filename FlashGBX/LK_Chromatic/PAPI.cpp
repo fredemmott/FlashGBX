884066d47461956cd9d375fafbe0c96f8c053c96
Fred Emmott <git@fredemmott.com>
2026-09-17 13:10:13 -0500

    Use singleton functions instead of static singletons
    Avoid potential static initializer issues

--- FlashGBX/LK_Chromatic/PAPI.cpp
+++ FlashGBX/LK_Chromatic/PAPI.cpp
@@ -117,8 +117,14 @@ private:
     const char* const _label;
 };
 
-ContiguousSPSCStream<8192> gPAPI_to_LK("PAPI-to-LK");
-ContiguousSPSCStream<8192> gLK_to_PAPI("LK-to-PAPI");;
+struct Streams_t {
+    ContiguousSPSCStream<8192> papi_to_lk { "PAPI-to-LK" };
+    ContiguousSPSCStream<8192> lk_to_papi { "LK-to-PAPI" };
+};
+Streams_t& streams() {
+    static Streams_t instance {};
+    return instance;
+}
 
 #ifdef _WIN32
 [[nodiscard]]
@@ -151,7 +157,7 @@ extern "C" LK_CHROMATIC_EXPORT void papi_recv_from_lk(uint8_t* data, const uint1
     SPAMMY(TraceLoggingWriteStart(tla, "papi_recv_from_lk()"));
 
 
-    gLK_to_PAPI.read(data, count);
+    streams().lk_to_papi.read(data, count);
 
     SPAMMY(TraceLoggingWriteStop(tla, "papi_recv_from_lk()"));
 }
@@ -173,7 +179,7 @@ extern "C" LK_CHROMATIC_EXPORT void papi_send_to_lk(uint8_t* data, const uint16_
                 while (!stop.stop_requested()) {
                     uint8_t cmd {};
                     {
-                        if (!gPAPI_to_LK.read(&cmd, 1, stop)) {
+                        if (!streams().papi_to_lk.read(&cmd, 1, stop)) {
                             haveWorker.clear();
                             UNSET_THREAD_NAME();
                             return;
@@ -190,7 +196,7 @@ extern "C" LK_CHROMATIC_EXPORT void papi_send_to_lk(uint8_t* data, const uint16_
         }.detach();
     }
 
-    gPAPI_to_LK.write(count, [src = data](uint8_t* const dst, const std::size_t n) {
+    streams().papi_to_lk.write(count, [src = data](uint8_t* const dst, const std::size_t n) {
         std::memcpy(dst, src, n);
     });
 
@@ -253,32 +259,32 @@ extern "C" void mc_on_error(const char* const str, const std::size_t length) {
 }
 
 extern "C" void papi_send_to_lk_reset_output_buffer() {
-    gPAPI_to_LK.clear();
+    streams().papi_to_lk.clear();
 }
 
 extern "C" void papi_send_to_lk_flush() {
-    while (gPAPI_to_LK.pending_count()) {
+    while (streams().papi_to_lk.pending_count()) {
         std::this_thread::sleep_for(std::chrono::milliseconds(1));
     }
 }
 
 extern "C" uint16_t papi_recv_from_lk_pending_count() {
-    return gLK_to_PAPI.pending_count();
+    return streams().lk_to_papi.pending_count();
 }
 
 extern "C" void papi_recv_from_lk_reset_input_buffer() {
-    gLK_to_PAPI.clear();
+    streams().lk_to_papi.clear();
 }
 
 ///// implement LK host IO functions using the PAPI buffers //////
 
 extern "C" void lk_send_to_host(const uint8_t* data, const uint16_t count) {
-    gLK_to_PAPI.write(count, [src = data](uint8_t* const dest, const std::size_t n) {
+    streams().lk_to_papi.write(count, [src = data](uint8_t* const dest, const std::size_t n) {
         std::memcpy(dest, src, n);
     });
 }
 
 
 extern "C" void lk_recv_from_host(uint8_t* data, const uint16_t count) {
-    gPAPI_to_LK.read(data, count);
+    streams().papi_to_lk.read(data, count);
 }
