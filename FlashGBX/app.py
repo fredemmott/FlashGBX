3612a62847740e8b84496bbceec35472bb6f39fb
Fred Emmott <git@fredemmott.com>
2026-09-19 17:11:35 -0500

    bump version

--- FlashGBX/app.py
+++ FlashGBX/app.py
@@ -6,7 +6,7 @@ import platform, importlib, re, sys
 
 class AppInfo:
 	NAME = "FlashGBX"
-	VERSION_PEP440 = "5.1+fredemmott.1"
+	VERSION_PEP440 = "5.1+fredemmott.2"
 	VERSION = "v{:s}".format(VERSION_PEP440)
 	VERSION_TIMESTAMP = 1788010533
 
